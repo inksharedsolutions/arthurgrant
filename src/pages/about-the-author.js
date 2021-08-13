ca96e01040ac80703d2df0e9822b9ff971d60eed
inksharedsolutions <webdev.inkshared@gmail.com>
2021-08-13 14:13:14 +0800

    upadtes

--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -12,7 +12,7 @@ const ATB = (props) => {
     return (
         <>
             <Layout>
-            <Helmet title="About the Author | Arthur Grant"/>
+                <Helmet title="About the Author | Arthur Grant" />
                 <Nav pathExt={props.path} />
 
                 <Banner
@@ -30,25 +30,28 @@ const ATB = (props) => {
 
                             <div className="heading-quote">
                                 <h4>
-                                "If ONE is for you, who dare be against you.”
+                                    "If ONE is for you, who dare be against you.”
                                 </h4>
-                                <span className="ata-span-fx">
-                                Author Quote
-                                </span>
+                                <p>
+                                    <span className="author-name">
+                                        {/* <span className="author-span-ft">Author | Writer </span> */}
+                                        <span className="ata-name">Arthur Grant</span>
+                                    </span>
+                                </p>
                             </div>
                         </div>
 
                         <article className="article-section column" id="author">
                             <p>
-                            Arthur Grant has worked in the education industry for the last twenty years. He has a Master’s Degree in Exceptionality Education, and has specifically worked with students that have special needs. He developed a passion for writing as a young student himself, winning writing competitions, writing music, and school productions. He states that he enjoys using what he has learned about cognitive development to create visuals through words, that will work in unison to stimulate a mental escape from your current world.
+                                Arthur Grant has worked in the education industry for the last twenty years. He has a Master’s Degree in Exceptionality Education, and has specifically worked with students that have special needs. He developed a passion for writing as a young student himself, winning writing competitions, writing music, and school productions. He states that he enjoys using what he has learned about cognitive development to create visuals through words, that will work in unison to stimulate a mental escape from your current world.
                             </p>
-
+{/* 
                             <p>
                                 <span className="author-name">
                                     <span className="author-span-ft">Author | Writer </span>
                                     <span className="ata-name">Arthur Grant</span>
                                 </span>
-                            </p>
+                            </p> */}
 
                         </article>
                     </section>
